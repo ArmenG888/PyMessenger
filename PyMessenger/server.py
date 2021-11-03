420cec3a63f574026cad460e74263f514426f488
Armen._.G <52843550+ArmenG888@users.noreply.github.com>
2021-11-02 18:26:38 -0700

    Create server.py

---
+++ PyMessenger/server.py
@@ -0,0 +1,23 @@
+import socket
+import threading
+import time
+import keyboard
+import random
+class server:
+	def __init__(self):
+		self.name = "Armen"
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+		s.bind(("127.0.0.1",52000))
+		s.listen(5)
+		self.conn, addr = s.accept()
+		recieve = threading.Thread(target=self.recieve)
+		recieve.start()
+		while True:
+			x = input(self.name+":")
+			self.conn.send(self.name.encode() +":".encode()+x.encode())
+	def recieve(self):
+		while True:
+			data = self.conn.recv(1024).decode()
+			print("\n"+data)
+app = server()
