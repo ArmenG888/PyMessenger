8c72589968ba6c2c1f7dbc3cab10568240a95da4
Armen._.G <52843550+ArmenG888@users.noreply.github.com>
2021-11-06 09:45:01 -0700

    Update server.py

--- PyMessenger/server.py
+++ PyMessenger/server.py
@@ -1,19 +1,39 @@
-import socket,threading,time,random
-class server:
+import socket,time,threading,random,sys
+from PySide2 import QtCore, QtGui, QtWidgets
+from PySide2.QtCore import (QCoreApplication, QPropertyAnimation, QDate, QDateTime, QMetaObject, QObject, QPoint, QRect, QSize, QTime, QUrl, Qt, QEvent)
+from PySide2.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont, QFontDatabase, QIcon, QKeySequence, QLinearGradient, QPalette, QPainter, QPixmap, QRadialGradient)
+from PySide2.QtWidgets import *
+from ui_PyMessanger import Ui_MainWindow
+
+class server(QMainWindow):
 	def __init__(self):
+		self.text = ""
 		self.name = "Armen"
-		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-		s.bind(("127.0.0.1",52000))
-		s.listen(5)
-		self.conn, addr = s.accept()
+		QMainWindow.__init__(self)
+		self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+		self.s.bind(("127.0.0.1",52001))
+		self.s.listen(5)
+		self.s, addr = self.s.accept()
+		self.username = self.s.recv(1024).decode()
+		self.s.send(self.name.encode())
+		self.ui = Ui_MainWindow()
 		recieve = threading.Thread(target=self.recieve)
 		recieve.start()
-		while True:
-			x = input(self.name+":")
-			self.conn.send(self.name.encode() +":".encode()+x.encode())
+		self.ui.setupUi(self)
+		self.ui.send_button.clicked.connect(self.send)
+		self.show()
 	def recieve(self):
 		while True:
-			data = self.conn.recv(1024).decode()
-			print("\n"+data)
-app = server()
+			data = self.s.recv(1024).decode()
+			self.text += self.username + ":"+ data + "\n"
+			self.ui.textEdit.setText(self.text)
+	def send(self):
+		self.text += self.name + ":" + self.ui.entry.toPlainText() + "\n"
+		self.ui.textEdit.setText(self.text)
+		self.s.send(self.ui.entry.toPlainText().encode())
+		self.ui.entry.setText("")
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = server()
+    sys.exit(app.exec_())
